240b3562f8273d2a9641483516fdb4430801d97d
Martin Streicher <martin.streicher@gmail.com>
2015-09-26 19:35:55 -0400

    Bump version.

--- spec/darksky/version_spec.rb
+++ spec/darksky/version_spec.rb
@@ -2,6 +2,6 @@ require 'spec_helper'
 
 describe 'Darksky::VERSION' do
   specify 'should be the correct version' do
-    expect(Darksky::VERSION).to eq('2.0.0')
+    expect(Darksky::VERSION).to eq('2.0.1')
   end
 end
